0bad5c0d3a9760bad061d44eda1180af6c3e1408
Geordi Dosher <cgartist15@gmail.com>
2016-02-09 21:34:27 -0600

    Finished slide template

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,6 @@
  * Created by Geordi on 11/27/2015.
  */
 
-//$(window).scroll(calculateHeaderPicScrollAnimation($));
-var headerIsUpTop = true;
-
 $(document).ready(function() {
     calculateHeadProfilePicVisibility()
 })
@@ -42,4 +39,33 @@ function topPosFromTop(element) {
 
 function botPosFromTop(element) {
     return topPosFromTop(element) + element.height();
+}
+
+/*
+ *   SLIDE MORE/LESS Animations
+ * */
+
+function showMorePress(btn) {
+    var slidePar = $(btn).parent().parent();
+
+    //Fade out 'less'
+    $(slidePar).find('.s_less').fadeOut();
+
+    //Fade in 'more'
+    var moreRoot = $(slidePar).find('.s_more');
+    $(moreRoot).fadeIn(600);
+
+    //Animate images
+    $(moreRoot).find('.s_more_images')
+        .delay(1000).animate(
+        {right: '10%'}, {
+            duration: 1000,
+            easing: 'easeOutCirc'});
+
+    //Animate content
+    $(moreRoot).find('.s_more_content')
+        .delay(1500).animate(
+        {left: '10%'}, {
+            duration: 1000,
+            easing: 'easeOutCirc'});
 }
